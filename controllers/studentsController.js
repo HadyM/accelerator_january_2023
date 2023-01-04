913d7ad31567611fa20fccc2b1f46fe8d101fdc0
Hady Mohamed <hadymohamed@pursuit.org>
2023-01-03 22:19:41 -0500

    Added get all students route and get student by id route

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const { response } = require("../app");
 const controller = express.Router();
 
+const studentData = require("../studentData.json");
+
 controller.get("/", (req, res) => {
-  res.json({ hello: "world" });
+  try {
+    res.json(studentData);
+  } catch (err) {
+    res.status(500).send("An Error Occurred");
+  }
+});
+
+controller.get("/:id", (req, res) => {
+  try {
+    const studentId = req.params.id;
+
+    if (!/[0-9]/.test(studentId)) {
+      res.send("Student id must be a number!");
+      return;
+    }
+
+    const singleStudent = studentData.students.find((student) => {
+      return student.id === studentId;
+    });
+
+    if (singleStudent) {
+      res.json(singleStudent);
+    } else {
+      res.send("No Student Available With That ID Number Please Try Again!");
+    }
+  } catch (err) {
+    res.status(500).send("An Error Occurred");
+  }
 });
 
 module.exports = controller;
