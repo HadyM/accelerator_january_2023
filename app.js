d5cba535006f5e5e3f93af39fb90e8fd3f08be22
Hady Mohamed <hadymohamed@pursuit.org>
2023-01-03 20:05:37 -0500

    created first route in app.js and created server.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+// import code for express
 const express = require("express");
 
+// initialize app
 const app = express();
 
+// route
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+// export app
 module.exports = app;
